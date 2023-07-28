9093e1c2c25444a0cc0b74f3ae36b3b0529ab3b1
David Vardzelian <davit.vardzelyan@softconstruct.com>
2023-07-28 13:39:45 +0400

    Add required projects and packages, also add DB context class.

---
+++ StripePaymentSystem/Shared/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public long Price { get; set; }
+    }
+}
